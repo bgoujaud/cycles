27657ee7c00f7f4ffd7d882f3d3ac48c46e45e34
baptiste <baptiste.goujaud@gmail.com>
2023-03-02 12:22:40 +0100

    Update grid_search

--- grid_search.py
+++ grid_search.py
@@ -13,13 +13,29 @@ from inexact_gradient_descent.contraction import wc_inexact_gradient_descent
 from inexact_gradient_descent.cycles import cycle_inexact_gradient_descent
 
 
-def make_grid_hb_and_nag(L, nb_points):
+def make_grid_hb(L, nb_points):
 
     beta_list = []
     alpha_list = []
 
     for beta in np.linspace(0, .99, nb_points):
         for alpha in np.arange(.05 / L, 1.95 * (1 + beta) / L, 2 / L / nb_points):
+            beta_list.append(beta)
+            alpha_list.append(alpha)
+
+    beta_list = np.array(beta_list)
+    alpha_list = np.array(alpha_list)
+
+    return beta_list, alpha_list
+
+
+def make_grid_nag(L, nb_points):
+
+    beta_list = []
+    alpha_list = []
+
+    for beta in np.linspace(0, .99, nb_points):
+        for alpha in np.arange(.05 / L, (0.95 + 1/(1+beta)) / L, 1.9 / L / nb_points):
             beta_list.append(beta)
             alpha_list.append(alpha)
 
@@ -45,19 +61,10 @@ def make_grid_gd_inexact(L, nb_points):
     return epsilon_list, gamma_list
 
 
-def grid_search_momentum(mu, L, nb_points, max_length, alg):
-    beta_list, alpha_list = make_grid_hb_and_nag(L=L, nb_points=nb_points)
+def grid_search_hb(mu, L, nb_points, max_length):
+    beta_list, alpha_list = make_grid_hb(L=L, nb_points=nb_points)
     cv_list = []
     cycle_list = []
-
-    if alg == "hb":
-        find_cv = wc_heavy_ball_momentum
-        find_cycle = cycle_heavy_ball_momentum
-    elif alg == "nag":
-        find_cv = wc_accelerated_gradient_strongly_convex
-        find_cycle = cycle_accelerated_gradient_strongly_convex
-    else:
-        raise Exception("Wrong alg")
 
     for i in tqdm(range(len(beta_list))):
         beta = beta_list[i]
@@ -66,9 +73,33 @@ def grid_search_momentum(mu, L, nb_points, max_length, alg):
         cycle = False
         for n in range(3, max_length + 1):
             if cv is False and cycle is False:
-                cv = find_cv(mu=mu, L=L, alpha=alpha, beta=beta, n=n, verbose=-1)
+                cv = wc_heavy_ball_momentum(mu=mu, L=L, alpha=alpha, beta=beta, n=n, verbose=-1)
             if cv is False and cycle is False:
-                cycle = find_cycle(mu=mu, L=L, alpha=alpha, beta=beta, n=n, threshold=10 ** -3, verbose=-1)
+                cycle = cycle_heavy_ball_momentum(mu=mu, L=L, alpha=alpha, beta=beta, n=n, threshold=10 ** -3, verbose=-1)
+        cv_list.append(cv)
+        cycle_list.append(cycle)
+
+    cv_list = np.array(cv_list)
+    cycle_list = np.array(cycle_list)
+
+    return beta_list, alpha_list, cv_list, cycle_list
+
+
+def grid_search_nag(mu, L, nb_points, max_length):
+    beta_list, alpha_list = make_grid_nag(L=L, nb_points=nb_points)
+    cv_list = []
+    cycle_list = []
+
+    for i in tqdm(range(len(beta_list))):
+        beta = beta_list[i]
+        alpha = alpha_list[i]
+        cv = False
+        cycle = False
+        for n in range(3, max_length + 1):
+            if cv is False and cycle is False:
+                cv = wc_accelerated_gradient_strongly_convex(mu=mu, L=L, alpha=alpha, beta=beta, n=n, verbose=-1)
+            if cv is False and cycle is False:
+                cycle = cycle_accelerated_gradient_strongly_convex(mu=mu, L=L, alpha=alpha, beta=beta, n=n, threshold=10 ** -3, verbose=-1)
         cv_list.append(cv)
         cycle_list.append(cycle)
 
@@ -105,7 +136,7 @@ def grid_search_inexact_grad(mu, L, nb_points, max_length):
 def get_graphic(mu, L, nb_points, max_length, alg):
 
     if alg == "hb":
-        beta_list, alpha_list, cv_list, cycle_list = grid_search_momentum(mu, L, nb_points, max_length, alg="hb")
+        beta_list, alpha_list, cv_list, cycle_list = grid_search_hb(mu, L, nb_points, max_length)
         plt.figure(figsize=(15, 9))
         plt.plot(alpha_list[cv_list], beta_list[cv_list], '.g')
         plt.plot(alpha_list[cycle_list], beta_list[cycle_list], '.r')
@@ -113,7 +144,7 @@ def get_graphic(mu, L, nb_points, max_length, alg):
         plt.savefig("hb.png")
 
     elif alg == "nag":
-        beta_list, alpha_list, cv_list, cycle_list = grid_search_momentum(mu, L, nb_points, max_length, alg="nag")
+        beta_list, alpha_list, cv_list, cycle_list = grid_search_nag(mu, L, nb_points, max_length)
         plt.figure(figsize=(15, 9))
         plt.plot(alpha_list[cv_list], beta_list[cv_list], '.g')
         plt.plot(alpha_list[cycle_list], beta_list[cycle_list], '.r')
@@ -135,6 +166,6 @@ def get_graphic(mu, L, nb_points, max_length, alg):
 if __name__ == "__main__":
 
     def get_the_right_graphic(alg):
-        get_graphic(mu=.1, L=1., nb_points=10, max_length=8, alg=alg)
+        get_graphic(mu=.1, L=1., nb_points=10, max_length=10, alg=alg)
 
     Parallel(n_jobs=3)(delayed(get_the_right_graphic)(alg) for alg in ["hb", "nag", "gd"])
