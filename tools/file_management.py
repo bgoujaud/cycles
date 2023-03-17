c69c0fd089b11a87876846f0030683489e904b50
baptiste <baptiste.goujaud@gmail.com>
2023-03-17 19:46:10 +0100

    Update file management to get nice figures

--- tools/file_management.py
+++ tools/file_management.py
@@ -1,26 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# from matplotlib.backends.backend_pgf import FigureCanvasPgf
-#
-# mpl.backend_bases.register_backend("pdf", FigureCanvasPgf)
-# size = 19
-# mpl.rcParams.update({
-#     "pgf.texsystem": "pdflatex",
-#     "font.family": "serif",
-#     "font.serif": "Times",
-#     "text.usetex": True,
-#     "pgf.rcfonts": False,
-#     "font.size": size,
-#     "axes.labelsize": size,
-#     "axes.titlesize": size,
-#     "figure.titlesize": size,
-#     "xtick.labelsize": size,
-#     "ytick.labelsize": size,
-#     "legend.fontsize": size,
-# })
-import scienceplots
-plt.style.use('science')
+from matplotlib import cm
+import matplotlib as mpl
+import matplotlib.lines as mlines
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes, mark_inset
+
+size = 19
+mpl.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    "font.family": "serif",
+    "font.serif": "Times",
+    "text.usetex": True,
+    "pgf.rcfonts": False,
+    "font.size": size,
+    "axes.labelsize": size,
+    "axes.titlesize": size,
+    "figure.titlesize": size,
+    "xtick.labelsize": size,
+    "ytick.labelsize": size,
+    "legend.fontsize": size,
+})
 
 
 def read_result_file(file_path):
@@ -59,18 +58,28 @@ def bound(method, L, beta):
 
 
 def get_colored_graphics(method, mu, L, max_cycle_length, folder="results/"):
-    plt.figure(figsize=(15, 9))
+    fig = plt.figure(figsize=(15, 9))
+    ax = plt.subplot(111)
 
-    gammas_lyap, betas_lyap = read_result_file(file_path=folder + "lyapunov/{}_mu{:.2f}_L{:.0f}.txt".format(method, mu, L))
+    if method == "HB":
+        axins = zoomed_inset_axes(ax, zoom=3.5, loc="lower right")
+
+    gammas_lyap, betas_lyap = read_result_file(
+        file_path=folder + "lyapunov/{}_mu{:.2f}_L{:.0f}.txt".format(method, mu, L))
     x_green = list()
     y_green = list()
     for gamma_max, beta in zip(gammas_lyap, betas_lyap):
         x_green += list(np.linspace(0, gamma_max, 500))
         y_green += [beta] * 500
-    plt.plot(x_green, y_green, '.', color="yellowgreen")
-    legends = ["convergence"]
+    ax.plot(x_green, y_green, '.', color="yellowgreen", label="convergence")
 
-    color_map = plt.get_cmap('OrRd')
+    if method == "HB":
+        axins.plot(x_green, y_green, ".", color="yellowgreen")
+
+    legends = ["convergence"]
+    colors = ["yellowgreen"]
+
+    color_map = plt.get_cmap('YlOrRd')
     for K in range(max_cycle_length, 1, -1):
         try:
             gammas_cycle, betas_cycle = read_result_file(
@@ -82,25 +91,38 @@ def get_colored_graphics(method, mu, L, max_cycle_length, folder="results/"):
                     x_red += list(np.linspace(gamma_min, bound(method, L, beta), 500))
                     y_red += [beta] * 500
             color_scale = (max_cycle_length + 1 - K) / (max_cycle_length - 1)
-            color = color_map((2 + 3 * color_scale) / 5)
-            plt.plot(x_red, y_red, '.', color=color)
+            color = color_map(color_scale)
+
+            ax.plot(x_red, y_red, '.', color=color, label="cycle of length {}".format(K))
+            if method == "HB":
+                axins.plot(x_red, y_red, ".", color=color)
             legends.append("cycle of length {}".format(K))
+            colors.append(color)
         except FileNotFoundError:
             pass
+    if method == "HB":
+        x1 = -0.01
+        x2 = 0.25
+        y1 = 0.95
+        y2 = 1.002
+        axins.set_xlim(x1, x2)
+        axins.set_ylim(y1, y2)
+        axins.set_xticks([])
+        axins.set_yticks([])
+        mark_inset(ax, axins, loc1=1, loc2=2, fc="none", ec="0.7")
+        ax.plot([x1, x1, x2, x2, x1], [y1, y2, y2, y1, y1], linewidth=0.6, color="grey")
 
-    if method == "GD":
-        best_loc = "lower left"
-    elif method == "HB":
-        best_loc = "lower right"
-    elif method == "NAG":
-        best_loc = "upper right"
-    elif method == "TOS":
-        best_loc = "upper right"
-    else:
-        raise ValueError
-
-    plt.legend(legends, loc=best_loc)
-    plt.savefig(folder + "figures/{}_mu{:.2f}_L{:.0f}_colored.png".format(method, mu, L))
+    bounds = list(range(len(colors[1:])))
+    norm = mpl.colors.BoundaryNorm(np.array(bounds) + 2, len(colors[1:]))
+    position = fig.add_axes([0.28, 0.92, 0.6, 0.02])  # [x_init, y_init, width, height]
+    clbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap=mpl.colors.ListedColormap(colors[1:][::-1])),
+                         cax=position, orientation="horizontal", shrink=0.8, fraction=0.1, aspect=50)
+    clbar.ax.set_title("Length of the shortest cycle")
+    handles = [mlines.Line2D([], [], color=colors[0], marker="s",
+                             linestyle="None", markersize=12, markeredgecolor="black")]
+    labels = [""]
+    fig.legend(handles, labels, bbox_to_anchor=(0.25, 1), title_fontsize=20, title="Convergence", frameon=False)
+    plt.savefig(folder + "figures/{}_mu{:.2f}_L{:.0f}_colored.png".format(method, mu, L), bbox_inches="tight")
 
 
 if __name__ == "__main__":
